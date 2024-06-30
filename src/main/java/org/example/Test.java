21ef4d277eb75ce8cfdef0b6c0949c9bfc5de763
xfxxf <842811047@qq.com>
2024-06-30 10:52:35 +0800

    主にIntersection.javaとTest.javaをコミット

--- src/main/java/org/example/Test.java
+++ src/main/java/org/example/Test.java
@@ -4,7 +4,7 @@ public class Test {
     public static void main(String[] args) {
         for (int i = 1; i <= 9; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(j + "*" + j + "=" + (i*j) + " ");
+                System.out.println(j + "*" + j + "=" + (i*j) + " ");
             }
             System.out.println();
         }
